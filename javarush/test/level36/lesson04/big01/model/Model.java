de6da701a1df1fae198091323507efd32d9fe3d0
Alexandr Shevtsov <hack85sh@gmail.com>
2016-12-29 03:10:00 +0400

    level36.lesson04.big01 Задание_1 commit

---
+++ javarush/test/level36/lesson04/big01/model/Model.java
@@ -0,0 +1,9 @@
+package com.javarush.test.level36.lesson04.big01.model;
+
+/**
+ * Created by sas on 29.12.16.
+ */
+public interface Model {
+    ModelData getModelData();
+    void loadUsers();
+}
---
+++ javarush/test/level36/lesson04/big01/model/ModelData.java
@@ -0,0 +1,21 @@
+package com.javarush.test.level36.lesson04.big01.model;
+
+import com.javarush.test.level36.lesson04.big01.bean.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by sas on 29.12.16.
+ */
+public class ModelData {
+    private List<User> users = new ArrayList<>();
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
