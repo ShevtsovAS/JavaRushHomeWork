bd1afe57471141f2929150c68bd2782e67de0845
Alexandr Shevtsov <hack85sh@gmail.com>
2017-01-10 20:07:45 +0400

    level37.lesson04.big01 Задание3 commit

---
+++ javarush/test/level37/lesson04/big01/female/TeenGirl.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level37.lesson04.big01.female;
+
+import com.javarush.test.level37.lesson04.big01.Human;
+
+/**
+ * Created by alexandr on 10.01.17.
+ */
+public class TeenGirl implements Human {
+    public static final int MAX_AGE = 19;
+
+    @Override
+    public String toString() {
+        return "TeenGirl{}";
+    }
+}
