d8cd5895c18cea2a5f5557f303749feb25469031
Alexandr Shevtsov <hack85sh@gmail.com>
2017-01-10 20:00:45 +0400

    level37.lesson04.big01 Задание2 commit

---
+++ javarush/test/level37/lesson04/big01/Solution.java
@@ -0,0 +1,15 @@
+package com.javarush.test.level37.lesson04.big01;
+
+import com.javarush.test.level37.lesson04.big01.male.MaleFactory;
+
+/**
+ * Created by alexandr on 10.01.17.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        MaleFactory factory = new MaleFactory();
+        System.out.println(factory.getPerson(99));
+        System.out.println(factory.getPerson(4));
+        System.out.println(factory.getPerson(15));
+    }
+}
