87ba5050f29bbdbad355af6c730bef33fc393be5
Alexandr Shevtsov <hack85sh@gmail.com>
2017-01-10 20:27:00 +0400

    level37.lesson04.big01 Задание4 commit

--- javarush/test/level37/lesson04/big01/male/MaleFactory.java
+++ javarush/test/level37/lesson04/big01/male/MaleFactory.java
@@ -1,11 +1,12 @@
 package com.javarush.test.level37.lesson04.big01.male;
 
+import com.javarush.test.level37.lesson04.big01.AbstractFactory;
 import com.javarush.test.level37.lesson04.big01.Human;
 
 /**
  * Created by alexandr on 10.01.17.
  */
-public class MaleFactory {
+public class MaleFactory implements AbstractFactory {
     public Human getPerson(int age) {
         if (age <= KidBoy.MAX_AGE ) return new KidBoy();
         else if (age <= TeenBoy.MAX_AGE) return new TeenBoy();
