7e018c2688312b56eb29b39a2ab45b0bfc3ce778
Alexandr Shevtsov <hack85sh@gmail.com>
2017-02-01 10:31:33 +0400

    level39.lesson09.big01 Задание6 commit

--- javarush/test/level39/lesson09/big01/LogParser.java
+++ javarush/test/level39/lesson09/big01/LogParser.java
@@ -11,6 +11,8 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LogParser implements IPQuery, UserQuery, DateQuery, EventQuery, QLQuery {
     private Path logDir;
@@ -510,12 +512,124 @@ public class LogParser implements IPQuery, UserQuery, DateQuery, EventQuery, QLQ
         Set<Object> objects = new HashSet<>();
 
         switch (query) {
-            case "get ip": objects.addAll(getUniqueIPs(null, null)); break;
-            case "get user": objects.addAll(getAllUsers()); break;
-            case "get date": objects.addAll(getAllDates()); break;
-            case "get event": objects.addAll(getAllEvents(null, null)); break;
-            case "get status": objects.addAll(getAllStatus()); break;
+            case "get ip": objects.addAll(getUniqueIPs(null, null)); return objects;
+            case "get user": objects.addAll(getAllUsers()); return objects;
+            case "get date": objects.addAll(getAllDates()); return objects;
+            case "get event": objects.addAll(getAllEvents(null, null)); return objects;
+            case "get status": objects.addAll(getAllStatus()); return objects;
         }
+
+        if (!isQueryValid(query)) {
+            System.out.println("Query is invalid!");
+            return null;
+        }
+
+        String[] vars = query.split("\\s");
+        String field1 = "";
+        String field2 = "";
+        String value1;
+
+        if (vars.length >= 2) field1 = vars[1];
+        if (vars.length >= 6) field2 = vars[3];
+
+        List<String> values = new ArrayList<>();
+        Pattern valuePattern = Pattern.compile("\".+?\"");
+        Matcher valueMatcher = valuePattern.matcher(query);
+
+        while (valueMatcher.find()) values.add(valueMatcher.group().replaceAll("\"", ""));
+
+        if (values.isEmpty()) {
+            System.out.println("Query is invalid!");
+            return null;
+        }
+
+        value1 = values.get(0);
+
+        if (field1.equals("ip")) {
+            switch (field2) {
+                case "user":
+                    objects.addAll(getIPsForUser(value1, null, null));
+                    break;
+                case "date":
+                    Date date = getDate(value1);
+                    objects.addAll(getUniqueIPs(date, date));
+                    break;
+                case "event":
+                    objects.addAll(getIPsForEvent(getEvent(value1), null, null));
+                    break;
+                case "status":
+                    Status status = Status.valueOf(value1);
+                    objects.addAll(getIPsForStatus(status, null, null));
+                    break;
+            }
+        }
+
+        if (field1.equals("user")) {
+            switch (field2) {
+                case "ip":
+                    objects.addAll(getUsersForIP(value1, null, null));
+                    break;
+                case "date":
+                    Date date = getDate(value1);
+                    objects.addAll(getUsersForDate(date));
+                    break;
+                case "event":
+                    Event event = getEvent(value1);
+                    objects.addAll(getUsersForEvent(event, null, null));
+                    break;
+                case "status":
+                    Status status = Status.valueOf(value1);
+                    objects.addAll(getUsersForStatus(status));
+                    break;
+            }
+        }
+
+        if (field1.equals("date")) {
+            switch (field2) {
+                case "ip":
+                    objects.addAll(getDatesForIp(value1));
+                    break;
+                case "user":
+                    Event[] events = Event.values();
+                    for (Event event : events) {
+                        objects.addAll(getDatesForUserAndEvent(value1, event, null, null));
+                    }
+                    break;
+                case "event":
+                    Set<String> users = getAllUsers();
+                    Event event = getEvent(value1);
+                    for (String user : users) {
+                        objects.addAll(getDatesForUserAndEvent(user, event, null, null));
+                    }
+                    break;
+                case "status":
+                    Status status = Status.valueOf(value1);
+                    objects.addAll(getDatesForStatus(status, null, null));
+                    break;
+            }
+        }
+
+        if (field1.equals("event")) {
+            switch (field2) {
+                case "ip": objects.addAll(getEventsForIP(value1, null, null)); break;
+                case "user": objects.addAll(getEventsForUser(value1, null, null)); break;
+                case "date":
+                    Date date = getDate(value1);
+                    objects.addAll(getEventsForDate(date));
+                    break;
+                case "status": objects.addAll(getEventsForStatus(Status.valueOf(value1), null, null));
+            }
+        }
+
+        if (field1.equals("status")) {
+            switch (field2) {
+                case "ip": objects.addAll(getStatusesForIp(value1)); break;
+                case "user": objects.addAll(getStatusesForUser(value1)); break;
+                case "date": objects.addAll(getStatusesForDate(getDate(value1))); break;
+                case "event": objects.addAll(getStatusesForEvent(getEvent(value1))); break;
+            }
+        }
+
         return objects;
     }
 
@@ -622,6 +736,151 @@ public class LogParser implements IPQuery, UserQuery, DateQuery, EventQuery, QLQ
             if (columns.length < 5) continue;
 
             statuses.add(Status.valueOf(columns[4]));
+        }
+
+        return statuses;
+    }
+
+    private boolean isQueryValid(String query) {
+        String[] vars = query.split("\\s");
+        if (vars.length == 2) return vars[0].equals("get");
+        if (vars.length >= 6) return vars[0].equals("get") && vars[2].equals("for");
+        return false;
+    }
+
+    private Set<String> getUsersForDate(Date date) {
+        Set<String> users = new HashSet<>();
+
+        for (String line : fileLines) {
+            String[] columns = line.trim().split("\\t");
+
+            if (columns.length < 5) continue;
+
+            String user = columns[1];
+            Date currentDate = getDate(columns[2]);
+
+            if (currentDate.getTime() == date.getTime()) users.add(user);
+        }
+
+        return users;
+    }
+
+    private Set<String> getUsersForStatus(Status status) {
+        Set<String> users = new HashSet<>();
+
+        for (String line : fileLines) {
+            String[] columns = line.trim().split("\\t");
+
+            if (columns.length < 5) continue;
+
+            String user = columns[1];
+            Status currentStatus = Status.valueOf(columns[4]);
+
+            if (currentStatus.equals(status)) users.add(user);
+        }
+
+        return users;
+    }
+
+    private Set<Date> getDatesForIp(String ip) {
+        Set<Date> dates = new HashSet<>();
+
+        for (String line : fileLines) {
+            String[] columns = line.trim().split("\\t");
+
+            if (columns.length < 5) continue;
+
+            String currentIp = columns[0];
+            Date currentDate = getDate(columns[2]);
+
+            if (currentIp.equals(ip)) dates.add(currentDate);
+        }
+
+        return dates;
+    }
+
+    private Set<Event> getEventsForDate(Date date) {
+        Set<Event> events = new HashSet<>();
+
+        for (String line : fileLines) {
+            String[] columns = line.trim().split("\\t");
+
+            if (columns.length < 5) continue;
+
+            Date currentDate = getDate(columns[2]);
+            Event event = getEvent(columns[3]);
+
+            if (currentDate.getTime() == date.getTime()) events.add(event);
+        }
+
+        return events;
+    }
+
+    private Set<Status> getStatusesForIp(String ip) {
+        Set<Status> statuses = new HashSet<>();
+
+        for (String line : fileLines) {
+            String[] columns = line.trim().split("\\t");
+
+            if (columns.length < 5) continue;
+
+            String currentIp = columns[0];
+            Status status = Status.valueOf(columns[4]);
+
+            if (currentIp.equals(ip)) statuses.add(status);
+        }
+
+        return statuses;
+    }
+
+    private Set<Status> getStatusesForUser(String user) {
+        Set<Status> statuses = new HashSet<>();
+
+        for (String line : fileLines) {
+            String[] columns = line.trim().split("\\t");
+
+            if (columns.length < 5) continue;
+
+            String currentUser = columns[1];
+            Status status = Status.valueOf(columns[4]);
+
+            if (currentUser.equals(user)) {
+                statuses.add(status);
+            }
+        }
+
+        return statuses;
+    }
+
+    private Set<Status> getStatusesForDate(Date date) {
+        Set<Status> statuses = new HashSet<>();
+
+        for (String line : fileLines) {
+            String[] columns = line.trim().split("\\t");
+
+            if (columns.length < 5) continue;
+
+            Date currentDate = getDate(columns[2]);
+            Status status = Status.valueOf(columns[4]);
+
+            if (currentDate.getTime() == date.getTime()) statuses.add(status);
+        }
+
+        return statuses;
+    }
+
+    private Set<Status> getStatusesForEvent(Event event) {
+        Set<Status> statuses = new HashSet<>();
+
+        for (String line : fileLines) {
+            String[] columns = line.trim().split("\\t");
+
+            if (columns.length < 5) continue;
+
+            Event currentEvent = getEvent(columns[3]);
+            Status status = Status.valueOf(columns[4]);
+
+            if (currentEvent.equals(event)) statuses.add(status);
         }
 
         return statuses;
